e6ef08ec8fd1dd6edc597010e5e86118b202b0cd
RoughTomato <baklazanzaglady@gmail.com>
2023-10-10 12:42:09 +1000

    Added delete

--- BinaryTree/BinaryTree/BinaryTree/BinaryTree.cs
+++ BinaryTree/BinaryTree/BinaryTree/BinaryTree.cs
@@ -4,9 +4,26 @@ public class Node<T> {
     public T? data;
     public Node<T>? left;
     public Node<T>? right;
+
+    public Node(){}
+    public Node(T data) => this.data = data;
 }
 
-public class BinaryTree {
+public class BinaryTree<T> {
+    public readonly Node<T>? root;
 
+    public BinaryTree(Node<T> root) => this.root = root;
+    public BinaryTree(T data) => this.root = new Node<T>(data);
 
+    public void Delete(ref Node<T> node) {
+        if (node.right != null) {
+            node = node.right;
+        }
+        else if (node.left != null) {
+            node = node.left;
+        }
+        else {
+            node = null; 
+        }
+    }
 }
--- BinaryTree/BinaryTree/BinaryTree/Program.cs
+++ BinaryTree/BinaryTree/BinaryTree/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BinaryTree;
+
+public class Program {
+    private static void Main(string[] args) {
+        var bt = new BinaryTree<int>(1);
+        bt.root.left = new Node<int>(2);
+        bt.root.left.left = new Node<int>(3);
+
+        bt.Delete(ref bt.root.left);
+
+        Console.WriteLine(bt.root.left.data);
+    }
+}
