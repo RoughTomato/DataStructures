559f1d49016dbfe9a38b30ea3808920be848d994
RoughTomato <baklazanzaglady@gmail.com>
2023-10-10 17:11:11 +1000

    Created Node<T> for LinkedList<T>

--- DataStructures/LinkedList/LinkedList.cs
+++ DataStructures/LinkedList/LinkedList.cs
@@ -1,9 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
 namespace DataStructures.LinkedList;
-class LinkedList {
+
+class Node<T> {
+    public T? data;
+    public Node<T>? next = null;
+
+    public Node(T? data) {
+        this.data = data;
+    }
+
+    public Node() {
+        this.data = default(T);
+    }
+}
+
+class LinkedList<T> : ICollection<T> {
+    public Int32 Count => throw new NotImplementedException();
+
+    public Boolean IsReadOnly => throw new NotImplementedException();
+
+    public void Add(T item) {
+        throw new NotImplementedException();
+    }
+
+    public void Clear() {
+        throw new NotImplementedException();
+    }
+
+    public Boolean Contains(T item) {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(T[] array, Int32 arrayIndex) {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<T> GetEnumerator() {
+        throw new NotImplementedException();
+    }
+
+    public Boolean Remove(T item) {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() {
+        throw new NotImplementedException();
+    }
 }
