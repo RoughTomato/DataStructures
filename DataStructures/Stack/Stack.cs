a8f4950654ea339d97a341e815facb4385670bc4
RoughTomato <baklazanzaglady@gmail.com>
2023-10-11 14:36:27 +1000

    very basic stack

---
+++ DataStructures/Stack/Stack.cs
@@ -0,0 +1,39 @@
+﻿namespace DataStructures.Stack;
+class Stack<T> {
+
+    private T[] elements;
+    private int top;
+    private int max;
+
+    private Stack(int size) {
+        this.elements = new T[size];
+        this.top = -1;
+        this.max = size;
+    }
+    
+    public int Size() {
+        return top;
+    }
+
+    public T Pop() {
+        if (this.top == -1) {
+            throw new StackOverflowException();
+        }
+
+        return this.elements[this.top--];
+    }
+
+    public void Push(T item) {
+        if (this.top == this.max - 1) {
+            throw new StackOverflowException();
+        }
+        else {
+            this.elements[++this.top] = item;
+        }
+    }
+
+    public T Peek()
+    {
+        return elements[this.top];
+    }
+}
