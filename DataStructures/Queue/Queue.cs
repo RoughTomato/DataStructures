9575e2553b6eb7db5746c29c455a512cbebd26c7
RoughTomato <baklazanzaglady@gmail.com>
2023-10-12 16:00:53 +1000

    Queue

--- DataStructures/Queue/Queue.cs
+++ DataStructures/Queue/Queue.cs
@@ -3,18 +3,43 @@
 
 
 public class Queue<T> {
-    private T head;
-    private T tail;
+    private T[] queue;
+    private int head;
+    private int tail;
     private int size;
 
     public Queue() { }
 
-    public void Enqueue(T data) {
+    public Queue(int size = 10) {
+        this.size = size;
+        queue = new T[size];
+    }
 
+    public void Enqueue(T element) {
+        if (this.tail > this.size)
+            throw new Exception("Queue Overflow");
+        this.queue[this.tail] = element;
+        if (this.tail == this.size)
+            this.tail = 1;
+        else
+            this.tail++;
     }
 
     public T Dequeue() {
-        return null;
+        T element = this.queue[this.head];
+        if (this.head == this.size)
+            this.head = 1;
+        else
+            this.head++;
+        return element;
+    }
+
+    public T Peek() {
+        if (this.size > 0) {
+            throw new IndexOutOfRangeException();
+        }
+
+        return this.queue[this.head];
     }
 
 }
